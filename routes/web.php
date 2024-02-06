bb2f50b40cd4bbee636d816674ff7af2144c7443
elmu35b <musabgaili@gmail.com>
2024-02-06 22:39:48 +0300

    all

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // User::factory()->create();
+    return view('welcome');
+});
+
+
+
+Route::domain('{name}.example.com')->group(function () {
+    Route::get('users', [UserController::class ,'index']);
+});
